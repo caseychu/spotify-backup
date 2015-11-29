dafc8b08c7b08e2b4ed146ccff4d0617935dbe52
Alexandre L <alex131089@msn.com>
2015-11-29 17:26:23 +0100

    Separate MD/Txt, export more data, allow json indent
    Separate MD/Txt : new option for format
    Export more data : add profile, albums, tracks, collaborative playlists
    Allow json indent : to have pretty output
    Todo : txt/MD output for all data

--- spotify-backup.py
+++ spotify-backup.py
@@ -20,7 +20,7 @@ class SpotifyAPI:
 		self._auth = auth
 	
 	# Gets a resource from the Spotify API and returns the object.
-	def get(self, url, params={}, tries=3):
+	def get(self, url, params={}, tries=3, root=''):
 		# Construct the correct URL.
 		if not url.startswith('https://api.spotify.com/v1/'):
 			url = 'https://api.spotify.com/v1/' + url
@@ -34,7 +34,10 @@ class SpotifyAPI:
 				req.add_header('Authorization', 'Bearer ' + self._auth)
 				res = urllib.request.urlopen(req)
 				reader = codecs.getreader('utf-8')
-				return json.load(reader(res))
+				response = json.load(reader(res))
+				if root: 
+					response = response[root]
+				return response
 			except Exception as err:
 				log('Couldn\'t load URL: {} ({})'.format(url, err))
 				time.sleep(2)
@@ -43,12 +46,15 @@ class SpotifyAPI:
 	
 	# The Spotify API breaks long lists into multiple pages. This method automatically
 	# fetches all pages and joins them, returning in a single list of objects.
-	def list(self, url, params={}):
-		response = self.get(url, params)
+	def list(self, url, params={}, root=''):
+		response = self.get(url, params, root=root)
 		items = response['items']
 		while response['next']:
 			response = self.get(response['next'])
 			items += response['items']
+			print('.', end='')
+			sys.stdout.flush()
+		print()
 		return items
 	
 	# Pops open a browser window for a user to log in and authorize API access.
@@ -110,9 +116,9 @@ class SpotifyAPI:
 		def __init__(self, access_token):
 			self.access_token = access_token
 
-def log(str):
+def log(str, end="\n"):
 	#print('[{}] {}'.format(time.strftime('%I:%M:%S'), str).encode(sys.stdout.encoding, errors='replace'))
-	sys.stdout.buffer.write('[{}] {}\n'.format(time.strftime('%I:%M:%S'), str).encode(sys.stdout.encoding, errors='replace'))
+	sys.stdout.buffer.write(('[{}] {}'+end).format(time.strftime('%I:%M:%S'), str).encode(sys.stdout.encoding, errors='replace'))
 	sys.stdout.flush()
 
 def main():
@@ -120,9 +126,11 @@ def main():
 	parser = argparse.ArgumentParser(description='Exports your Spotify playlists. By default, opens a browser window '
 	                                           + 'to authorize the Spotify Web API, but you can also manually specify'
 	                                           + ' an OAuth token with the --token option.')
-	parser.add_argument('--token', metavar='OAUTH_TOKEN', help='use a Spotify OAuth token (requires the '
+	parser.add_argument('-t', '--token', metavar='OAUTH_TOKEN', help='use a Spotify OAuth token (requires the '
 	                                           + '`playlist-read-private` permission)')
-	parser.add_argument('--format', default='txt', choices=['json', 'txt'], help='output format (default: txt)')
+	parser.add_argument('-f', '--format', default='json', choices=['json', 'txt', 'md'], help='output format (default: json)')
+	parser.add_argument('-l', '--load', metavar='JSON_FILE', help='load an existing json file to create txt or markdown output (playlists only currently)')
+	parser.add_argument('-i', '--indent', metavar='INDENT_STR', default=None, help='indent JSON output')
 	parser.add_argument('file', help='output filename', nargs='?')
 	args = parser.parse_args()
 	
@@ -130,41 +138,82 @@ def main():
 	while not args.file:
 		args.file = input('Enter a file name (e.g. playlists.txt): ')
 	
-	# Log into the Spotify API.
-	if args.token:
-		spotify = SpotifyAPI(args.token)
+	if args.load:
+		with open(args.load, 'r', encoding='utf-8') as f:
+			data = json.load(f)
 	else:
-		spotify = SpotifyAPI.authorize(client_id='5c098bcc800e45d49e476265bc9b6934', scope='playlist-read-private')
-	
-	# Get the ID of the logged in user.
-	me = spotify.get('me')
-	log('Logged in as {display_name} ({id})'.format(**me))
+		# Log into the Spotify API.
+		if args.token:
+			spotify = SpotifyAPI(args.token)
+		else:
+			spotify = SpotifyAPI.authorize(client_id='5c098bcc800e45d49e476265bc9b6934', scope='user-follow-read user-library-read playlist-read-private playlist-read-collaborative')
+		
+		# me								https://developer.spotify.com/web-api/get-current-users-profile/
+		# follow['artists]	https://developer.spotify.com/web-api/get-followed-artists/
+		# albums						https://developer.spotify.com/web-api/get-users-saved-albums/
+		# tracks						https://developer.spotify.com/web-api/get-users-saved-tracks/
+		# playlists					https://developer.spotify.com/web-api/console/get-playlists/?user_id=wizzler
+		data = {}
+		
+		# Get the ID of the logged in user.
+		data['me'] = spotify.get('me')
+		log('Logged in as {display_name} ({id})'.format(**data['me']))
 
-	# List all playlists and all track in each playlist.
-	playlists = spotify.list('users/{user_id}/playlists'.format(user_id=me['id']), {'limit': 50})
-	for playlist in playlists:
-		log('Loading playlist: {name} ({tracks[total]} songs)'.format(**playlist))
-		playlist['tracks'] = spotify.list(playlist['tracks']['href'], {'limit': 100})
+		# Get follows - scope user-follow-read
+		# "root" workaround for non-consistent API ..
+		data['following'] = {}
+		following = spotify.get('me/following', {'type': 'artist', 'limit': 1}, root='artists')
+		log('Loading followed artists: {total} artists'.format(**following), end='')
+		data['following']['artists'] = spotify.list('me/following', {'type': 'artist', 'limit': 50}, root='artists')
+		
+		# List saved albums - scope user-library-read
+		albums = spotify.get('me/albums', {'limit': 1})
+		log('Loading saved albums: {total} albums'.format(**albums), end='')
+		data['albums'] = spotify.list('me/albums', {'limit': 50})
+		
+		# List saved tracks - scope user-library-read
+		tracks = spotify.get('me/tracks', {'limit': 1})
+		log('Loading tracks: {total} songs'.format(**tracks), end='')
+		data['tracks'] = spotify.list('me/tracks', {'limit': 50})
+		
+		# List all playlists and all track in each playlist - scope playlist-read-private, playlist-read-collaborative
+		data['playlists'] = spotify.list('users/{user_id}/playlists'.format(user_id=data['me']['id']), {'limit': 50})
+		for playlist in data['playlists']:
+			log('Loading playlist: {name} ({tracks[total]} songs)'.format(**playlist))
+			playlist['tracks'] = spotify.list(playlist['tracks']['href'], {'limit': 100})
 	
 	# Write the file.
 	with open(args.file, 'w', encoding='utf-8') as f:
 		# JSON file.
 		if args.format == 'json':
-			json.dump(playlists, f)
+			json.dump(data, f, indent=args.indent)
 		
-		# Tab-separated file. Test
+		# Tab-separated file.
 		elif args.format == 'txt':
-			f.write("# Spotify Playlists Backup " + time.strftime("%d %b %Y") + "\r\n")
-			for playlist in playlists:
-				f.write("## " + playlist["name"] + "\r\n")
+			for playlist in data['playlists']:
+				f.write(playlist['name'] + "\n")
 				for track in playlist['tracks']:
-					f.write("* {name}\t{artists}\t{album}\t`{uri}`\r\n".format(
+					f.write('{name}\t{artists}\t{album}\t{uri}\n'.format(
+						uri=track['track']['uri'],
+						name=track['track']['name'],
+						artists=', '.join([artist['name'] for artist in track['track']['artists']]),
+						album=track['track']['album']['name']
+					))
+				f.write('\n')
+		
+		# Markdown
+		elif args.format == 'md':
+			f.write("# Spotify Playlists Backup " + time.strftime("%d %b %Y") + "\n")
+			for playlist in data['playlists']:
+				f.write("## " + playlist["name"] + "\n")
+				for track in playlist['tracks']:
+					f.write("* {name}\t{artists}\t{album}\t`{uri}`\n".format(
 						uri=track["track"]["uri"],
 						name=track["track"]["name"],
 						artists=", ".join([artist["name"] for artist in track["track"]["artists"]]),
 						album=track["track"]["album"]["name"]
 					))
-				f.write("\r\n")
+				f.write("\n")
 	log('Wrote file: ' + args.file)
 
 if __name__ == '__main__':
